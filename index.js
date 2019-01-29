94f56128267f1a284b9eddab0ac1c4d038592522
Juan Antonio <ruchin10@gmail.com>
2019-01-29 23:30:05 +0100

    added route and mongoose model

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import tweets from './routes/tweets';
+import config from './config'
 
 var app = express()
 
@@ -7,3 +9,5 @@ app.listen(config.server.port, () => {
 });
 
 app.use(express.static('public'));
+
+app.use('/tweets', tweets);
