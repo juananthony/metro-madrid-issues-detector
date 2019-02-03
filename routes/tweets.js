2bb54e0d7f6e0133f67664405e9436b6b7e82e08
Juan Antonio <ruchin10@gmail.com>
2019-02-03 18:25:47 +0100

    new style for official tweets

--- routes/tweets.js
+++ routes/tweets.js
@@ -6,14 +6,22 @@ const tweetsRouter = express.Router();
 
 const parseUrlEncoded = bodyParser.urlencoded({extended:true});
 
-tweetsRouter.route('/userTweets')
+tweetsRouter.route('/')
     .get((request, response) => {
-        console.log("GET /tweets/userTweets")
+        console.log("GET /tweets")
         var query = tweetModel.find(
             {
-                '$or': [{'classification.naive_bayes.result': 'issue'}, {'classification.naive_bayes.result': 'complaint'}],
-                'user.id': {'$ne': 182764833},
-                'text': {$regex: '^[^RT ](.*)$'}
+                '$or': [
+                    {'$and': [
+                        {'$or': [{'classification.naive_bayes.result': 'issue'}, {'classification.naive_bayes.result': 'complaint'}]},
+                        {'user.id': {'$ne': 182764833}},
+                        {'text': {$regex: '^[^RT ](.*)$'}}
+                    ]},
+                    {'$and': [
+                        {'user.id': 182764833},
+                        {'text': {$regex: '^Circulac(.*)|^Servicio(.*)|^Incidencia(.*)|^Continúa interr(.*)|^Servicio norm(.*)|^El tramo interr(.*)|^Los trenes no efect(.*)|^Interrumpida la cir(.*)|^Continúa interrum(.*)|^Servicio restablecido(.*)'}}
+                    ]}
+                ],
             },
             {
                 'created_at': 1,
@@ -27,122 +35,6 @@ tweetsRouter.route('/userTweets')
             },
             {
                 sort: {'timestamp_ms': -1}
-            });
-        query.exec((err, tweets) => {
-            console.log("received ... " + tweets.length)
-            if(tweets !== undefined) {
-                response.json(tweets);
-            } else {
-                response.json('{error: "no tweets"}');
-            }
-        })
-    });
-
-tweetsRouter.route('/issues')
-    .get((request, response) => {
-        console.log("GET /tweets/issues")
-        var query = tweetModel.find(
-            {
-                'classification.naive_bayes.result': 'issue',
-                'user.id': {'$ne': 182764833},
-                'text': {$regex: '^[^RT ](.*)$'}
-            },
-            {
-                'created_at': 1,
-                'text': 1,
-                'extended_tweet':1,
-                'timestamp_ms':1,
-                'user': 1,
-                'id': 1,
-                'id_str': 1
-            },
-            {
-                sort: {'timestamp_ms': -1}
-            });
-        query.exec((err, tweets) => {
-            console.log("received ... " + tweets.length)
-            if(tweets !== undefined) {
-                response.json(tweets);
-            } else {
-                response.json('{error: "no tweets"}');
-            }
-        })
-    });
-
-tweetsRouter.route('/complaints')
-    .get((request, response) => {
-        console.log("GET /tweets/complaints")
-        var query = tweetModel.find(
-            {
-                'classification.naive_bayes.result': 'complaint',
-                'user.id': {'$ne': 182764833},
-                'text': {$regex: '^[^RT ](.*)$'}
-            },
-            {
-                'created_at': 1,
-                'text': 1,
-                'extended_tweet':1,
-                'timestamp_ms':1,
-                'user': 1,
-                'id': 1,
-                'id_str': 1
-            });
-        query.exec((err, tweets) => {
-            console.log("received ... " + tweets.length)
-            if(tweets !== undefined) {
-                response.json(tweets);
-            } else {
-                response.json('{error: "no tweets"}');
-            }
-        })
-    });
-
-tweetsRouter.route('/nothings')
-    .get((request, response) => {
-        console.log("GET /tweets/nothings")
-        var query = tweetModel.find(
-            {
-                'classification.naive_bayes.result': 'nothing',
-                'user.id': {'$ne': 182764833},
-                'text': {$regex: '^[^RT ](.*)$'}
-            },
-            {
-                'created_at': 1,
-                'text': 1,
-                'extended_tweet':1,
-                'timestamp_ms':1,
-                'user': 1,
-                'id': 1,
-                'id_str': 1
-            });
-        query.exec((err, tweets) => {
-            console.log("received ... " + tweets.length)
-            if(tweets !== undefined) {
-                response.json(tweets);
-            } else {
-                response.json('{error: "no tweets"}');
-            }
-        })
-    });
-
-tweetsRouter.route('/official')
-    .get((request, response) => {
-        console.log("GET /tweets/official")
-        var query = tweetModel.find(
-            {
-                '$or': [{'classification.naive_bayes.result': 'issue'}, {'classification.naive_bayes.result': 'complaint'}],
-                'user.id': 182764833,
-                'text': {$regex: '^[^RT ](.*)$'},
-                'in_reply_to_status_id': null
-            },
-            {
-                'created_at': 1,
-                'text': 1,
-                'extended_tweet':1,
-                'timestamp_ms':1,
-                'user': 1,
-                'id': 1,
-                'id_str': 1
             });
         query.exec((err, tweets) => {
             console.log("received ... " + tweets.length)
