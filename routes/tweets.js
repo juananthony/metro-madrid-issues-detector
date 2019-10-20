d833ec6a1bb892b3cf0db103bf657ba47b079ea7
Juan Antonio <ruchin10@gmail.com>
2019-10-20 22:59:08 +0200

    order changed

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import tweetModel from '../models/tweet';
+import moment from 'moment';
 
 const tweetsRouter = express.Router();
 
@@ -35,8 +36,8 @@ tweetsRouter.route('/')
                 'classification.naive_bayes.result': 1
             },
             {
-                sort: {'timestamp_ms': -1}
-            }).limit(50);
+                sort: {'_id': -1}
+            });
         query.exec((err, tweets) => {
             console.log("received ... " + tweets.length)
             if(tweets !== undefined) {
@@ -46,5 +47,53 @@ tweetsRouter.route('/')
             }
         })
     });
+
+
+tweetsRouter.route("/:timestamp/:page/:days").get((request, response) => {
+        console.log("GET /tweets")
+        var timestamp = request.params.timestamp,
+            page = request.params.page,
+            days = request.params.days,
+            firstDay = moment.unix(timestamp),
+            lastDay = firstDay.startOf("day").subtract(days, "days");
+
+        var query = tweetModel.find(
+            {
+                '$or': [
+                    {'$and': [
+                        {'$or': [{'classification.naive_bayes.result': 'issue'}, {'classification.naive_bayes.result': 'complaint'}]},
+                        {'user.id': {'$ne': 182764833}},
+                        {'text': {$regex: '^[^RT ](.*)$'}}
+                    ]},
+                    {'$and': [
+                        {'user.id': 182764833},
+                        {'text': {$regex: '^Circulac(.*)|^Servicio(.*)|^Incidencia(.*)|^Continúa interr(.*)|^Servicio norm(.*)|^El tramo interr(.*)|^Los trenes no efect(.*)|^Interrumpida la cir(.*)|^Continúa interrum(.*)|^Servicio restablecido(.*)'}}
+                    ]}
+                ],
+                {'timestamp_ms' }
+            },
+            {
+                'text': 1,
+                'extended_tweet.full_text':1,
+                'timestamp_ms':1,
+                'user.id': 1,
+                'user.name': 1,
+                'user.screen_name': 1,
+                'user.profile_image_url_https': 1,
+                'id_str': 1,
+                'classification.naive_bayes.result': 1
+            },
+            {
+                sort: {'_id': -1}
+            });
+        query.exec((err, tweets) => {
+            console.log("received ... " + tweets.length)
+            if(tweets !== undefined) {
+                response.json(tweets);
+            } else {
+                response.json('{error: "no tweets"}');
+            }
+        })
+    })
 
 module.exports = tweetsRouter;
